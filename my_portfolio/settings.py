d6a3c23c59a845bd9255b343c7568b7f75c61293
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-07-06 22:47:37 -0400

    Config deployement

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
+
+import django_heroku
+django_heroku.settings(locals())
